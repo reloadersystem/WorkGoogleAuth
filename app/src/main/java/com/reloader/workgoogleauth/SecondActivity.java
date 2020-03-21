f12e27a61fae50a768a9802cd81bf5dd0f0d34e2
reloader <37155445+reloadersystem@users.noreply.github.com>
2020-03-21 12:53:33 -0500

    probando api java

--- app/src/main/java/com/reloader/workgoogleauth/SecondActivity.java
+++ app/src/main/java/com/reloader/workgoogleauth/SecondActivity.java
@@ -2,6 +2,7 @@ package com.reloader.workgoogleauth;
 
 import android.net.Uri;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
@@ -9,12 +10,35 @@ import android.widget.TextView;
 import android.widget.Toast;
 
 import com.bumptech.glide.Glide;
+import com.bumptech.glide.request.RequestOptions;
 import com.google.android.gms.auth.api.signin.GoogleSignIn;
 import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
 import com.google.android.gms.auth.api.signin.GoogleSignInClient;
 import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
+import com.google.api.client.extensions.java6.auth.oauth2.AuthorizationCodeInstalledApp;
+import com.google.api.client.extensions.jetty.auth.oauth2.LocalServerReceiver;
+import com.google.api.client.googleapis.auth.oauth2.GoogleAuthorizationCodeFlow;
+import com.google.api.client.googleapis.auth.oauth2.GoogleClientSecrets;
+import com.google.api.client.googleapis.javanet.GoogleNetHttpTransport;
+import com.google.api.client.http.HttpRequestInitializer;
+import com.google.api.client.http.javanet.NetHttpTransport;
+import com.google.api.client.json.JsonFactory;
+import com.google.api.client.json.jackson2.JacksonFactory;
+import com.google.api.client.util.store.FileDataStoreFactory;
+import com.google.api.services.classroom.Classroom;
+import com.google.api.services.classroom.ClassroomScopes;
+import com.google.api.services.classroom.model.Course;
+import com.google.api.services.classroom.model.ListCoursesResponse;
+
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.security.GeneralSecurityException;
+import java.util.Collections;
+import java.util.List;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
@@ -24,6 +48,11 @@ public class SecondActivity extends AppCompatActivity {
     TextView name, email, id;
     Button signOut;
     GoogleSignInClient mGoogleSignInClient;
+    private static final JsonFactory JSON_FACTORY = JacksonFactory.getDefaultInstance();
+    private static final String CREDENTIALS_FILE_PATH = "/credentials.json";
+    private static final List<String> SCOPES = Collections.singletonList(ClassroomScopes.CLASSROOM_COURSES_READONLY);
+    private static final String TOKENS_DIRECTORY_PATH = "tokens";
+    private static final String APPLICATION_NAME = "Google Classroom API Java Quickstart";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -55,7 +84,36 @@ public class SecondActivity extends AppCompatActivity {
             name.setText(personName);
             email.setText(personEmail);
             id.setText(personId);
-            Glide.with(this).load(String.valueOf(personPhoto)).into(imageView);
+            Glide.with(this)
+                    .load(String.valueOf(personPhoto))
+                    .apply(RequestOptions.circleCropTransform())
+                    .into(imageView);
+
+            try {
+                final NetHttpTransport HTTP_TRANSPORT = new NetHttpTransport();
+                Classroom service = new Classroom.Builder(HTTP_TRANSPORT, JSON_FACTORY, (HttpRequestInitializer) getCredentials(HTTP_TRANSPORT))
+                        .setApplicationName(APPLICATION_NAME)
+                        .build();
+                ListCoursesResponse response = service.courses().list()
+                        .setPageSize(10)
+                        .execute();
+
+                List<Course> courses = response.getCourses();
+                if (courses == null || courses.size() == 0) {
+                    Log.e("Coursefail", "No courses found.");
+                } else {
+                    System.out.println("Courses:");
+                    for (Course course : courses) {
+                        Log.e("Coursesdata: ", "\n" + course.getName());
+                    }
+                }
+
+            } catch (IOException e) {
+                e.printStackTrace();
+                Log.e("errorTrace: ", e.toString());
+
+            }
+
 
         }
 
@@ -68,6 +126,23 @@ public class SecondActivity extends AppCompatActivity {
         });
 
 
+    }
+
+    private Object getCredentials(final NetHttpTransport HTTP_TRANSPORT) throws IOException {
+        InputStream in = SecondActivity.class.getResourceAsStream(CREDENTIALS_FILE_PATH);
+        if (in == null) {
+            throw new FileNotFoundException("Resource not found: " + CREDENTIALS_FILE_PATH);
+        }
+        GoogleClientSecrets clientSecrets = GoogleClientSecrets.load(JSON_FACTORY, new InputStreamReader(in));
+
+        // Build flow and trigger user authorization request.
+        GoogleAuthorizationCodeFlow flow = new GoogleAuthorizationCodeFlow.Builder(
+                HTTP_TRANSPORT, JSON_FACTORY, clientSecrets, SCOPES)
+                .setDataStoreFactory(new FileDataStoreFactory(new java.io.File(TOKENS_DIRECTORY_PATH)))
+                .setAccessType("offline")
+                .build();
+        LocalServerReceiver receiver = new LocalServerReceiver.Builder().setPort(8888).build();
+        return new AuthorizationCodeInstalledApp(flow, receiver).authorize("user");
     }
 
     private void signOutGoogle() {

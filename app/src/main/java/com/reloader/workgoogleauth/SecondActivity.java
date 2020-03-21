d23101cd4190395aad84fba7552e4cc80be1fd91
reloader <37155445+reloadersystem@users.noreply.github.com>
2020-03-21 11:30:27 -0500

    Initial commit

---
+++ app/src/main/java/com/reloader/workgoogleauth/SecondActivity.java
@@ -0,0 +1,88 @@
+package com.reloader.workgoogleauth;
+
+import android.net.Uri;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.bumptech.glide.Glide;
+import com.google.android.gms.auth.api.signin.GoogleSignIn;
+import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
+import com.google.android.gms.auth.api.signin.GoogleSignInClient;
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class SecondActivity extends AppCompatActivity {
+    ImageView imageView;
+    TextView name, email, id;
+    Button signOut;
+    GoogleSignInClient mGoogleSignInClient;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_second);
+
+        GoogleSignInOptions gso = new GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+                .requestEmail()
+                .build();
+
+        mGoogleSignInClient = GoogleSignIn.getClient(this, gso);
+
+
+        imageView = findViewById(R.id.imageView);
+        name = findViewById(R.id.txtName);
+        email = findViewById(R.id.txtEmail);
+        id = findViewById(R.id.txtID);
+        signOut = findViewById(R.id.button);
+
+        GoogleSignInAccount acct = GoogleSignIn.getLastSignedInAccount(this);
+        if (acct != null) {
+            String personName = acct.getDisplayName();
+            String personGivenName = acct.getGivenName();
+            String personFamilyName = acct.getFamilyName();
+            String personEmail = acct.getEmail();
+            String personId = acct.getId();
+            Uri personPhoto = acct.getPhotoUrl();
+
+            name.setText(personName);
+            email.setText(personEmail);
+            id.setText(personId);
+            Glide.with(this).load(String.valueOf(personPhoto)).into(imageView);
+
+        }
+
+
+        signOut.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                signOutGoogle();
+            }
+        });
+
+
+    }
+
+    private void signOutGoogle() {
+
+        mGoogleSignInClient.signOut()
+                .addOnCompleteListener(this, new OnCompleteListener<Void>() {
+                    @Override
+                    public void onComplete(@NonNull Task<Void> task) {
+//                           Intent intent = new Intent(SecondActivity.this,  MainActivity.class);
+//                           startActivity(intent);
+
+                        Toast.makeText(SecondActivity.this, "Sesión Cerrada Exitosamente", Toast.LENGTH_SHORT).show();
+                        finish();
+                    }
+                });
+
+    }
+}
